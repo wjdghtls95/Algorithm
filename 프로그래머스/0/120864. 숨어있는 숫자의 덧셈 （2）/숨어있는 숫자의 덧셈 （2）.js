89dc0aa14cd0f24a51f0ac0b4666e161d6be26c7
wjdghtls95 <81152647+wjdghtls95@users.noreply.github.com>
2024-07-27 00:24:00 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -0,0 +1,10 @@
+function solution(my_string) {
+    var answer = 0;
+     const nums = my_string.match(/[0-9]+/g);
+        
+    return nums ? nums
+                      .map(num => Number(num))
+                      .reduce( (a, c) => a + c, 0) 
+                : 0;
+
+}
