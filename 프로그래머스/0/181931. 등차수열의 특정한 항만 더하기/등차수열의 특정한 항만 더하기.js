81754e4fa6b96d129786bcabd244d70a66f76a3b
wjdghtls95 <81152647+wjdghtls95@users.noreply.github.com>
2025-08-06 18:07:21 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
@@ -1,23 +1,9 @@
 function solution(a, d, included) {
     let answer = 0;
 
-    const terms = [];
-    let term = a;
-
     for (let i = 0; i < included.length; i++) {
-        terms.push(term)
-
-        term += d;
-    }
-    console.log(terms);
-
-    if (terms.length !== included.length) {
-        throw new Error('wrong terms count');
-    }
-
-    for (let i = 0; i < included.length; i++) {
-        if (included[i] === true) {
-            answer += terms[i]
+        if (included[i]) {
+            answer += a + d * i;
         }
     }
 
