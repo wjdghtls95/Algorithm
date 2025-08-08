77237ed1e257e36d4f9e9d405ad25681b77ea855
wjdghtls95 <81152647+wjdghtls95@users.noreply.github.com>
2025-08-08 18:54:02 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    const answer = [];
+
+    while (n !== 1) {
+        answer.push(n);
+        if (n % 2 === 0) {
+            n = n / 2;
+        } else {
+            n = 3 * n + 1;
+        }
+    }
+
+    answer.push(1)
+
+    return answer;
+}
