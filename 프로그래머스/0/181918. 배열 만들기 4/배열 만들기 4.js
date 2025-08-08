69cc79de8f5eb8cb29b87d15883a7ae1656f08e4
wjdghtls95 <81152647+wjdghtls95@users.noreply.github.com>
2025-08-08 19:27:27 +0900

    [level 0] Title: 배열 만들기 4, Time: 3.95 ms, Memory: 38.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,24 @@
+function solution(arr) {
+    const stk = [];
+    let i = 0;
+
+    while (i < arr.length) {
+        if (stk.length === 0) {
+            stk.push(arr[i]);
+            i++;
+            continue;
+        }
+
+        if (stk[stk.length - 1] < arr[i]) {
+            stk.push(arr[i]);
+            i++;
+            continue;
+        }
+
+        if (stk[stk.length - 1] >= arr[i]) {
+            stk.pop();
+        }
+    }
+
+    return stk;
+}
