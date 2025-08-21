b1af213ce356bc0e234aac82e694e0b9406139a0
wjdghtls95 <81152647+wjdghtls95@users.noreply.github.com>
2025-08-21 14:47:33 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.17 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,18 @@
+function solution(num_list) {
+    let odd = 0;
+    let even = 0;
+
+    for (let i = 0; i < num_list.length; i++) {
+        if (i % 2 === 0) {
+            even += num_list[i]
+        } else {
+            odd += num_list[i]
+        }
+    }
+
+    if (even >= odd){
+        return even
+    } else {
+        return odd
+    }
+}
