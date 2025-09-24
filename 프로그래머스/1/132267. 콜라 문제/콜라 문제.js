7164da623592706471a1640112e4ed1d1c8adb39
wjdghtls95 <81152647+wjdghtls95@users.noreply.github.com>
2025-09-24 17:24:08 +0900

    [level 1] Title: 콜라 문제, Time: 0.23 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -1,12 +1,19 @@
+/***
+ * a = 마트에 가져다줄 빈 병 개수
+ * b = 마트에서 주는 병 개수 <- a 개를 줘야 b 병을 줌
+ * n = 처음에 가지고 있는 병 개수
+ * return = 주고 받고 하면서 총 받을 수 있는 병의 개수
+ */
+
 function solution(a, b, n) {
-    // 콜라 받기위해 주는 병수 a
-    // a 개가져다 주면 b병을 주는 마트
-    // 빈병 n개
-    var answer = 0;
-    while(n >= a) {
-        answer += Math.floor(n/a)*b
-        n = Math.floor(n/a)*b + n%a
+    let answer = 0;
+    while (n >= a) {
+        let exchanged = Math.floor(n / a) * b;  // 새로 받은 콜라
+        answer += exchanged;                     // 마신 콜라 누적
+        n = (n % a) + exchanged;                 // 남은 병(교환 못 한 병 + 새로 마신 콜라)
     }
-    
     return answer;
 }
+
+console.log(solution(2, 1, 20))
+console.log(solution(3, 1, 20))
