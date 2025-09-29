8fdaa7e3b64cff487475ab8a65154225e37500a7
wjdghtls95 <81152647+wjdghtls95@users.noreply.github.com>
2025-09-29 14:56:29 +0900

    [level 1] Title: 추억 점수, Time: 1.11 ms, Memory: 34.3 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -1,11 +1,22 @@
 function solution(name, yearning, photo) {
-    const personYearning = {};
-    
+    var answer = [] ;
+
+    let obj = new Map()
+
     for (let i = 0; i < name.length; i++) {
-        personYearning[name[i]] = yearning[i];
+        obj.set(name[i], yearning[i])
     }
-    
-     return photo.map((arr) =>
-        arr.reduce((acc, cur) => acc + (personYearning[cur] || 0), 0)
-  );   
+
+    for (const people of photo) {
+        let score = 0;
+
+        for(const person of people) {
+            score += obj.get(person) || 0;
+        }
+
+        answer.push(score);
+    }
+
+
+    return answer;
 }
