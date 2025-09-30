50a4b03dc401e3b70c223c875ee0a28deab66408
wjdghtls95 <81152647+wjdghtls95@users.noreply.github.com>
2025-09-30 14:16:25 +0900

    [level 2] Title: 예상 대진표, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,23 +1,11 @@
 function solution(n, a, b) {
     let round = 0;
+
     while (a !== b) {
         a = Math.ceil(a / 2);
         b = Math.ceil(b / 2);
         round++;
     }
+
     return round;
 }
-// function solution(n, a, b) {
-//   var cnt = 1; // 1라운드 부터 시작이므로 1로 시작 0으로 하면 x
-//   let nextNum = (num) => Math.floor((num + 1) / 2); // 이겼을때 다음으로 가질 번호
-//   while (a !== b) { // a 다음 번호가 b 다음번호와 같아지면 끝남
-//     if (nextNum(a) === nextNum(b)) {
-//       break;
-//     }
-//     a = nextNum(a);
-//     b = nextNum(b);
-//     console.log('a, b : ', a, b);
-//     cnt++
-//   }
-//   return cnt;
-// }
