9d47433aaa043ca03cf913b57bf564d51636c6de
wjdghtls95 <81152647+wjdghtls95@users.noreply.github.com>
2025-12-15 18:34:33 +0900

    [level 1] Title: 과일 장수, Time: 249.17 ms, Memory: 93.7 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,13 +1,21 @@
 function solution(k, m, score) {
-    var answer = 0;
-  
-  score.sort((a, b) => b - a);
-  
-  let boxCnt = Math.floor(score.length / m);
+    // 1. 점수 내림차순 정렬
+    score.sort((a, b) => b - a);
 
-  for(let i = 1; i <= boxCnt; i++) {
-    answer += score[m * i - 1] * m;
-  }
+    let answer = 0;
 
-  return answer;
+    // 2. m개씩 묶어서 상자 만들기
+    for (let i = 0; i < score.length; i += m) {
+        const box = score.slice(i, i + m); // 길이 m까지 자름
+
+        // 3. m개가 안 되면 상자 못 만드니까 종료
+        if (box.length < m) break;
+
+        // 4. 상자 가격 = 그 상자의 최솟값 * m
+        // 내림차순이라 box[box.length - 1] 이 최솟값
+        const boxMin = box[box.length - 1];
+        answer += boxMin * m;
+    }
+
+    return answer;
 }
