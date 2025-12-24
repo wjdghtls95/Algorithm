33a7da8db4caaa81336801ced44cc8e88427669f
wjdghtls95 <81152647+wjdghtls95@users.noreply.github.com>
2025-12-24 18:46:21 +0900

    [level 3] Title: 네트워크, Time: 0.44 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,24 +1,51 @@
+// function solution(n, computers) {
+//   const visited = Array(n).fill(false);
+//   let count = 0;
+
+//   for (let i = 0; i < n; i++) {
+//     if (visited[i]) continue;
+
+//     const q = [i];
+//     let head = 0;
+//     visited[i] = true;
+
+//     while (head < q.length) {
+//       const cur = q[head++]; // shift 대체
+//       for (let next = 0; next < n; next++) {
+//         if (computers[cur][next] === 1 && !visited[next]) {
+//           visited[next] = true; // 큐 넣기 전에 방문
+//           q.push(next);
+//         }
+//       }
+//     }
+//     count++;
+//   }
+//   return count;
+// }
+
 function solution(n, computers) {
-  const visited = Array(n).fill(false);
-  let count = 0;
+  const visited = new Set();
+  let networks = 0;
 
   for (let i = 0; i < n; i++) {
-    if (visited[i]) continue;
+    if (visited.has(i)) continue;
 
+    networks++;
     const q = [i];
     let head = 0;
-    visited[i] = true;
+    visited.add(i);
 
     while (head < q.length) {
-      const cur = q[head++]; // shift 대체
+      const cur = q[head++];
+
       for (let next = 0; next < n; next++) {
-        if (computers[cur][next] === 1 && !visited[next]) {
-          visited[next] = true; // 큐 넣기 전에 방문
+        if (computers[cur][next] === 1 && !visited.has(next)) {
+          visited.add(next);
           q.push(next);
         }
       }
     }
-    count++;
   }
-  return count;
+
+  return networks;
 }
